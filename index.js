46cf756e448cc38ea559d73cc0b6b0985d12b641
ABalisha <arditbalisha01@gmail.com>
2021-10-17 13:50:04 +0200

    test

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const express = require('express')
-const app = express();
+const app = express()
 const server = require('http').createServer(app)
 const socketio = require('socket.io')
 const path = require('path')
 const io = socketio(server)
-require('dotenv')
-app.use(express.static(path.join(__dirname, 'views')))
-io.on('connection',(socket)=>{
-console.log("Socket Connected")
-socket.emit('message', 'Welcome to Chat')
+app.get('/index.html',(req,res)=>{
+    res.sendFile(__dirname + '/views/index.html')
+    io.on('connection', socket=>
+    {
+        console.log(socket.id)
+        socket.on('messageFront',(message,room)=>{
+            if(room === ''){
+                socket.broadcast.emit('response',message)
+            }
+            else
+            {
+                socket.to(room).emit('response',message)
+            }
+        })
+        // Joining a custom room multiple people 
+        socket.on('join-room',room=>{
+            socket.join(room)
+        })
+        io.emit("message","A new user has been connected ")
 
-// Broadcast When a user connectis
-socket.broadcast.emit('message',"A user has connected");
-
-// runs when client disconnections
-socket.on('disconnect',()=>{
-    io.emit('message',"User has left the chat")
-})
-socket.on('chatMessage',(message)=>{
-    console.log(message)
-    io.emit('message',message)
-})
+    })
 })
 
-server.listen(process.env.PORT || 8888, ()=>{console.log("App Running")})
+
+server.listen(3000,()=>{console.log("App running on port 3000")})
